54b77a9519e8d668c8f117b447abb9cd9b7fa6d6
Steven Pollack <steven.pollack@proxima.capital>
2021-08-17 21:26:43 +1000

    update publish scripts

--- .github/parse_title.sh
+++ .github/parse_title.sh
@@ -2,7 +2,8 @@
 keyword=$(echo $1 | tr '[:upper:]' '[:lower:]')
 case $keyword in
     patch|minor|major|prepatch|preminor|premajor)
-    np $keyword --preview --allow-any --branch main
+    echo performing a $keyword
+    np --preview --no-tests --allow-any --branch main $keyword
     ;;
     *)
     np $keyword --preview --no-tests --allow-any --branch main
