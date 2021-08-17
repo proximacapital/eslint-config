e2da080774841717945ef90a7f120aaa1b26bcb2
stevenpollack <stevenpollack@users.noreply.github.com>
2021-08-17 21:33:02 +1000

    MAJOR work (#11)
    * workflow experiment
    
    * update workflow
    
    * update publish scripts
    
    * remove preview

---
+++ .github/parse_title.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+keyword=$(echo $1 | tr '[:upper:]' '[:lower:]')
+case $keyword in
+    patch|minor|major|prepatch|preminor|premajor)
+    echo performing a $keyword
+    np --no-tests --allow-any --branch main $keyword
+    ;;
+    *)
+    np $keyword --preview --no-tests --allow-any --branch main
+    ;;
+esac
