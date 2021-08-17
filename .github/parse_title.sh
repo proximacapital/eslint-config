db1a5a65d783129dccc40785e74cd80c92ad2718
Steven Pollack <steven.pollack@proxima.capital>
2021-08-17 21:32:05 +1000

    remove preview

--- .github/parse_title.sh
+++ .github/parse_title.sh
@@ -3,7 +3,7 @@ keyword=$(echo $1 | tr '[:upper:]' '[:lower:]')
 case $keyword in
     patch|minor|major|prepatch|preminor|premajor)
     echo performing a $keyword
-    np --preview --no-tests --allow-any --branch main $keyword
+    np --no-tests --allow-any --branch main $keyword
     ;;
     *)
     np $keyword --preview --no-tests --allow-any --branch main
