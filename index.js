53b24c8e2af71161d97dc3fc48185c2b679fcf35
Steven Pollack <steven.pollack@proxima.capital>
2021-08-18 14:27:22 +1000

    enforce ===

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ module.exports = {
         "key-spacing": "error",                                                                             // No space before colon in object literals
         "no-fallthrough": "error",                                                                          // Require explicit comment when switch cases fall through
         "no-implicit-coercion": "error",
+        "eqeqeq": "error", // enforce === and !===
         "import-newlines/enforce": [
             "error",
             {
