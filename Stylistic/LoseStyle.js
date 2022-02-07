774ae4057e872cde406412e919dff09ca3a0c1a7
Will Smith <williamqsmith11@gmail.com>
2022-02-07 18:13:23 -0500

    try this extends

--- Stylistic/LoseStyle.js
+++ Stylistic/LoseStyle.js
@@ -1,7 +1,7 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  extends: "./Stylistic/StrictStyle",
+  extends: "./StrictStyle.js",
   overrides: [
     {
       files: ["*.ts", "*.js"],
