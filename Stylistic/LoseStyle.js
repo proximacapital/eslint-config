d2cb8ffed6e9d933f96ac812e00f0343f046d89f
Will Smith <williamqsmith11@gmail.com>
2022-02-07 20:34:49 -0500

    include .ts in files

--- Stylistic/LoseStyle.js
+++ Stylistic/LoseStyle.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: "./StrictStyle.js",
   overrides: [
     {
-      files: ["*.js"],
+      files: ["*.js", "*.ts"],
       "parserOptions": {
         "project": [
           "./tsconfig.json"
